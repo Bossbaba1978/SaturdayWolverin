4fbf4fc4f6094ac50be7eac971d5ac933c6ab064
OWNER <owner@OWNERs-MacBook-Air.local>
2022-09-03 16:21:59 -0400

    first commit

---
+++ src/com/upskill/java/Class_0_HelloWorld.java
@@ -0,0 +1,15 @@
+package com.upskill.java;
+
+public class Class_0_HelloWorld 
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World,How you doing?");
+		
+		System.out.print("Hello World,This is Liaquat");
+		
+		int a = 5;
+		int b = 8;
+		int c = a+b;
+		
+				
